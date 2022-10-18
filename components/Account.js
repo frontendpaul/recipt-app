90ad90e8384acf394600bcb3027e5085f1e250be
frontendpaul <matizbrave@gmail.com>
2022-10-18 20:07:32 +0200

    display bills

--- components/Account.js
+++ components/Account.js
@@ -3,118 +3,34 @@ import { supabase } from '../utils/supabaseClient';
 
 export default function Account({ session }) {
   const [loading, setLoading] = useState(true);
-  const [username, setUsername] = useState(null);
-  const [website, setWebsite] = useState(null);
-  const [avatar_url, setAvatarUrl] = useState(null);
+  const [bills, setBills] = useState(null);
 
   useEffect(() => {
-    getProfile();
-  }, [session]);
+    getUsersBills();
+  }, []);
 
-  async function getCurrentUser() {
-    const {
-      data: { session },
-      error,
-    } = await supabase.auth.getSession();
-
-    if (error) {
-      throw error;
-    }
-
-    if (!session?.user) {
-      throw new Error('User not logged in');
-    }
-
-    return session.user;
-  }
-
-  async function getProfile() {
-    try {
-      setLoading(true);
-      const user = await getCurrentUser();
-
-      let { data, error, status } = await supabase
-        .from('profiles')
-        .select(`username, website, avatar_url`)
-        .eq('id', user.id)
-        .single();
-
-      if (error && status !== 406) {
-        throw error;
-      }
-
-      if (data) {
-        setUsername(data.username);
-        setWebsite(data.website);
-        setAvatarUrl(data.avatar_url);
-      }
-    } catch (error) {
-      alert(error.message);
-    } finally {
+  const getUsersBills = async () => {
+    setLoading(true);
+    let { data: bills, error } = await supabase
+      .from('bills')
+      .select(
+        `
+        *,
+        images (
+          *
+        )
+      `
+      )
+      .eq('user_id', session.user.id);
+    if (error) console.log('error', error);
+    else {
+      setBills(bills);
       setLoading(false);
     }
-  }
-
-  async function updateProfile({ username, website, avatar_url }) {
-    try {
-      setLoading(true);
-      const user = await getCurrentUser();
-
-      const updates = {
-        id: user.id,
-        username,
-        website,
-        avatar_url,
-        updated_at: new Date(),
-      };
-
-      let { error } = await supabase.from('profiles').upsert(updates);
-
-      if (error) {
-        throw error;
-      }
-    } catch (error) {
-      alert(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }
+  };
 
   return (
-    <div className="form-widget">
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={session.user.email} disabled />
-      </div>
-      <div>
-        <label htmlFor="username">Name</label>
-        <input
-          id="username"
-          type="text"
-          value={username || ''}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="website">Website</label>
-        <input
-          id="website"
-          type="website"
-          value={website || ''}
-          onChange={(e) => setWebsite(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <button
-          className="button primary block"
-          onClick={() => updateProfile({ username, website, avatar_url })}
-          disabled={loading}
-        >
-          {loading ? 'Loading ...' : 'Update'}
-        </button>
-      </div>
-
+    <div>
       <div>
         <button
           className="button block"
@@ -122,6 +38,10 @@ export default function Account({ session }) {
         >
           Sign Out
         </button>
+        {!loading &&
+          bills.map((bill) => {
+            return <div key={bill.id}>{bill.title}</div>;
+          })}
       </div>
     </div>
   );
